be461af209f5b2eca38622abc916ab01ec9165b2
Sushmitha <sushmithareddy0729@gmail.com>
2026-09-15 20:57:41 +0530

    Use safe login link for reviews

--- controllers/users.js
+++ controllers/users.js
@@ -50,9 +50,6 @@ module.exports.signup = async (req, res, next) => {
 };
 
 module.exports.renderLoginForm = (req, res) => {
-    if (req.query.redirect?.startsWith("/")) {
-        req.session.redirectUrl = req.query.redirect;
-    }
     res.render("users/login.ejs");
 };
 
